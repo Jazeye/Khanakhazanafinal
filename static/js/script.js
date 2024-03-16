a2efd6154e29adbc2433724cc5ca332ec62d6719
Jazeye <jaseelp394@gmail.com>
2024-03-16 22:52:28 +0530

    animation added

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,25 @@
 // search redirecting
-document.getElementById('searchForm').addEventListener('submit', function(event) {
+document
+  .getElementById("searchForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent default form submission
 
-    var query = document.getElementById('query').value; // Get the search query
+    var query = document.getElementById("query").value; // Get the search query
 
     // Redirect to the search URL with query string
-    window.location.href = "/search?query=" + encodeURIComponent(query) + "#section2";
+    window.location.href =
+      "/search?query=" + encodeURIComponent(query) + "#section2";
+  });
+
+// scrollrevel
+ScrollReveal({ 
+    reset: true, 
+    distance: '80px',
+    duration :2000,
+    delay:200
 });
+
+ScrollReveal().reveal('#section2,.sec1', { origin:'top' });
+ScrollReveal().reveal('#section2,#btn', { origin:'bottom' });
+
 
